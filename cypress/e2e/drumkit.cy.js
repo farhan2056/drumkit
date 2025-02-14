b02a11c53e1957583ef78246dab069108d37fcb3
Farhan <farhan2056@gmail.com>
2025-02-14 23:32:34 +0530

    cypress beatsequence to test

--- cypress/e2e/drumkit.cy.js
+++ cypress/e2e/drumkit.cy.js
@@ -1,7 +1,41 @@
+const beatSequence = [
+  // Bar 1
+  { key: 65, delay: 200 }, // Boom
+  { key: 70, delay: 50 }, // Kick
+  { key: 68, delay: 50 }, // Hi-hat
+  { key: 74, delay: 200 }, // Snare
+  { key: 68, delay: 50 }, // Hi-hat
+  
+  // Bar 2
+  { key: 65, delay: 200 }, // Boom
+  { key: 76, delay: 50 }, // Tom
+  { key: 68, delay: 50 }, // Hi-hat
+  { key: 74, delay: 200 }, // Snare
+  { key: 70, delay: 50 }, // Kick
+  { key: 68, delay: 50 }, // Hi-hat
+  
+  // Bar 3
+  { key: 65, delay: 200 }, // Boom
+  { key: 70, delay: 50 }, // Kick
+  { key: 68, delay: 50 }, // Hi-hat
+  { key: 74, delay: 200 }, // Snare
+  { key: 76, delay: 200 }, // Tom
+  
+  // Bar 4
+  { key: 65, delay: 200 }, // Boom
+  { key: 70, delay: 50 }, // Kick
+  { key: 68, delay: 50 }, // Hi-hat
+  { key: 74, delay: 200 }, // Snare
+  { key: 68, delay: 50 }, // Hi-hat
+  { key: 76, delay: 200 }, // Tom
+];
+
 describe('shows drumkit', () => {
     it('Displays the message in the list', () => {
       cy.visit('http://localhost:5173');
-        cy.get('[data-testid="70"]')
-            .click();
+      beatSequence.forEach(({ key, delay }) => {
+        cy.get(`[data-testid="${key}"]`).click();
+        cy.wait(delay);
+      });
     });
   });
